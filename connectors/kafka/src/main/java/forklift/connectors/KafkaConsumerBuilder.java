ac2ad91dc746485ce78a1a524e2ba39f51bb328a
Andrew Frieze <afrieze@sofi.org>
2017-03-15 16:07:41 -0600

    removed unused files

--- connectors/kafka/src/main/java/forklift/connectors/KafkaConsumerBuilder.java
+++
@@ -1,9 +0,0 @@
-package forklift.connectors;
-
-/**
- * Created by afrieze on 3/15/17.
- */
-public class KafkaConsumerBuilder {
-
-
-}
--- connectors/kafka/src/main/java/forklift/connectors/KafkaProducerBuilder.java
+++
@@ -1,7 +0,0 @@
-package forklift.connectors;
-
-/**
- * Created by afrieze on 3/15/17.
- */
-public class KafkaProducerBuilder {
-}
